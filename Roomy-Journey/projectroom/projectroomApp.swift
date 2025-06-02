01c4ac713df9f2293a8496381c19aca697b51145
Serrauomo <serdavids98@gmail.com>
2025-06-02 20:19:47 +0200

    Add files via upload

---
+++ Roomy-Journey/projectroom/projectroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectroomApp.swift
+//  projectroom
+//
+//  Created by Emanuele Bazzicalupo on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct projectroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
